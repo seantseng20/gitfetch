4c61faf1abbcd81ef038f69661c2caf69a817aca
SeanTseng2000 <f05350013@go.thu.edu.tw>
2023-09-16 18:45:54 +0800

    0.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React,{useState} from "react";
+import SearchForm from './SearchForm';
+
 
 function App() {
+  const [login,setLogin]=useState("seantseng20")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <SearchForm value={login} onSearch={setLogin} />
+    </>
   );
 }
 
